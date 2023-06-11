de31176a5c1160f33d1232ed1f89c5e9fe6a6630
Pema Zompa <pemazompa@hotmail.com>
2023-06-11 15:17:48 +0000

    Add logo and basic style to base html

--- main/settings.py
+++ main/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+
 ALLOWED_HOSTS = [
     '8000-pzompa-schoolofbuddhism-e7yk2gitian.ws-eu99.gitpod.io'
 ]
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
